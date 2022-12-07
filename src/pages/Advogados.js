964a6b58e245bc66504d3ac1b9b12b44833b125a
fernandoig1209 <fernandoig1209@gmail.com>
2022-12-07 11:24:48 -0300

    adicionada página de títulos

--- src/pages/Advogados.js
+++ src/pages/Advogados.js
@@ -22,28 +22,28 @@ function Advogados() {
     }
   }
   return (
-      <div className='container-fluid' style={{"borderRadius":"25px","margin":"1em","height":"100%", "width": "98%", "padding": "10px", "backgroundColor":"#fbfbfb","justifyContent":"center" }}>
-        {dadosAdvogados.length > 0 && (<table className='table table-striped table-hover'>
-          <thead>
-            <tr>
-              {colunas.map((colunas, i) => { return (<th scope='row' className='col-1 text-center' key={i}>{colunas.toUpperCase()}</th>) })}
-            </tr>
-          </thead>
-          <tbody>
-            {dadosAdvogados.map((elemento, i) => {
-              return (<tr>
-                {colunas.map(col => {
-                  return (
-                  <td className={style.tdin+' text-center col'}>
-                    {elemento[col].length>40?elemento[col].substring(0,40)+ "...":elemento[col]}
-                    
-                    </td>);
-                })}
-              </tr>)
-            })}
-          </tbody>
-        </table>)}
-      </div>
+    <div className='container-fluid' style={{ "borderRadius": "25px", "margin": "1em", "height": "100%", "width": "98%", "padding": "10px", "backgroundColor": "#fbfbfb", "justifyContent": "center" }}>
+      {dadosAdvogados.length > 0 && (<table className='table table-striped table-hover'>
+        <thead>
+          <tr>
+            {colunas.map((colunas, i) => { return (<th scope='row' className='col-1 text-center' key={i}>{colunas.toUpperCase()}</th>) })}
+          </tr>
+        </thead>
+        <tbody>
+          {dadosAdvogados.map((elemento, i) => {
+            return (<tr>
+              {colunas.map(col => {
+                return (
+                  <td className={style.tdin + ' text-center col'}>
+                    {elemento[col].length > 40 ? elemento[col].substring(0, 40) + "..." : elemento[col]}
+
+                  </td>);
+              })}
+            </tr>)
+          })}
+        </tbody>
+      </table>)}
+    </div>
   );
 }
 export default Advogados;
--- src/pages/Titulos.js
+++ src/pages/Titulos.js
@@ -1,4 +1,54 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import style from './Titulos.module.css';
+
 function Titulos(){
-  return(<>Titulos</>);
+  const [dadosTitulos, setDadosTitulos] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:7000/api/titulos/all')
+      .then((dados) => {
+        setDadosTitulos(dados.data);
+        console.log(dados.data)
+      })
+      .catch((erro) => {
+        console.log("não foi possível recuperar os dados da rota digitada")
+      })
+
+  }, [])
+
+  let colunas = [];
+
+  if (dadosTitulos !== []) {
+    for (const x in dadosTitulos[0]) {
+      colunas.push(x);
+    }
+  }
+  return(
+  
+    <div className='container-fluid' style={{"borderRadius":"25px","margin":"1em","height":"100%", "width": "98%", "padding": "10px", "backgroundColor":"#fbfbfb","justifyContent":"center" }}>
+    {dadosTitulos.length > 0 && (<table className='table table-striped table-hover'>
+      <thead>
+        <tr>
+          {colunas.map((colunas, i) => { return (<th scope='row' className='col-1 text-center' key={i}>{colunas.toUpperCase()}</th>) })}
+        </tr>
+      </thead>
+      <tbody>
+        {dadosTitulos.map((elemento, i) => {
+          return (<tr>
+            {colunas.map(col => {
+              return (
+              <td className={style.tdin+' text-center col'}>
+                {elemento[col].length>40?elemento[col].substring(0,40)+ "...":elemento[col]}
+                
+                </td>);
+            })}
+          </tr>)
+        })}
+      </tbody>
+    </table>)}
+  </div>
+
+    );
 }
 export default Titulos;
