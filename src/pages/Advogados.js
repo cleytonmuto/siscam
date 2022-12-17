cf93045e89aebe35ebd57285a8bb7577645e2c66
fernandoig1209 <fernandoig1209@gmail.com>
2022-12-17 19:14:40 -0300

    fix: alteração na responsividade das tabelas

--- src/pages/Advogados.js
+++ src/pages/Advogados.js
@@ -27,49 +27,55 @@ function Advogados() {
     }
   }
   return (
-    <><div className={style.fd}><Link to="/novoadvogado"><button type="button" class="btn btn-primary"><RxPlus />Cadastrar Advogados</button></Link></div>
-      <div className='container-fluid' style={{ "borderRadius": "15px", "margin": "1em", "height": "100%", "width": "98%", "padding": "10px", "backgroundColor": "#fff", "justifyContent": "center" }}>
-        {dadosAdvogados.length > 0 && (<><table className='table table-striped table-hover'
-          style={{ "backgroundColor": "#Fbfbfb", "borderRadius": "15px" }}>
-          <thead>
-            <tr>
-              {colunas.map((colunas, i) => { return (<th scope='row' className='col-1 text-center' key={i}>{colunas.toUpperCase()}</th>) })}
-              <th scope='row' className='col-1 text-center' key={210}><strong>AÇÕES</strong></th>
-            </tr>
-          </thead>
-          <tbody>
-            {dadosAdvogados.map((elemento, i) => {
-              return (<tr>
-                {colunas.map((col, i) => {
-                  return (<>
-                    <td className={style.tdin + ' text-center col'}>
-                      {elemento[col].length > 40 ? elemento[col].substring(0, 40) + "..." : elemento[col]}
-
-                    </td>
-
-                  </>);
-
-                })}
-                <td className='col-1 text-center '>
-                  <div className='acao'>
-                    <Link>
-                      <FiEdit /> Editar
-                    </Link>
-                  </div>
-                  <br />
-                  <div className='acao'>
-                    <Link >
-                      <VscEye /> Visualizar
-                    </Link>
-                  </div>
-
-                </td>
-              </tr>)
-            })}
-          </tbody>
-        </table></>)}
+    <div>
+      <div className={style.fd}><Link to="/novoadvogado">
+        <button type="button" class="btn btn-primary"><RxPlus />
+          Cadastrar Advogados
+        </button></Link>
       </div>
-    </>
+      <div className={'container-fluid ' + style.div_container} >
+        {dadosAdvogados.length > 0 && (<>
+          <div className='table-responsive'>
+            <table className='table table-striped table-hover'>
+              <thead>
+                <tr>
+                  {colunas.map((colunas, i) => { return (<th scope='row' className='col-1 text-center' key={i}>{colunas.toUpperCase()}</th>) })}
+                  <th scope='row' className='col-1 text-center' key={210}><strong>AÇÕES</strong></th>
+                </tr>
+              </thead>
+              <tbody>
+                {dadosAdvogados.map((elemento, i) => {
+                  return (<tr>
+                    {colunas.map((col, i) => {
+                      return (<>
+                        <td className={style.tdin + ' text-center col'}>
+                          {elemento[col].length > 40 ? elemento[col].substring(0, 40) + "..." : elemento[col]}
+
+                        </td>
+
+                      </>);
+
+                    })}
+                    <td className='text-center'>
+                      <span className='align-middle'>
+                        <Link>
+                          <FiEdit /> Editar
+                        </Link>
+                      </span>
+                      <span className='align-middle'>
+                        <Link >
+                          <VscEye /> Visualizar
+                        </Link>
+                      </span>
+                    </td>
+                  </tr>)
+                })}
+              </tbody>
+            </table>
+          </div>
+        </>)}
+      </div>
+    </div>
   );
 }
 export default Advogados;
--- src/pages/Titulos.js
+++ src/pages/Titulos.js
@@ -7,7 +7,7 @@ const LIMIT = 10
 function Titulos() {
 
   const [offset, setOffset] = useState(0);
-  const [page,setPage] = useState(0)
+  const [page, setPage] = useState(0)
 
   const [dadosTitulos, setDadosTitulos] = useState([]);
   const [totalTitulos, setTotalTitulos] = useState([]);
@@ -16,20 +16,20 @@ function Titulos() {
     axios.get(`http://localhost:7000/api/titulos/short?offset=${offset}&page=${page}&limit=${LIMIT}`)
       .then((dados) => {
         setDadosTitulos(dados.data);
-  
+
       })
       .catch((erro) => {
         console.log("não foi possível recuperar os dados da rota digitada")
       });
 
     axios.get(`http://localhost:7000/api/titulos/countRows`)
-    .then((dados)=>{
-      setTotalTitulos(dados.data.numLinhas);
-      console.log("fsad",dados.data.numLinhas);
-     
-    }) .catch((erro) => {
-      console.log("não foi possível recuperar os dados da rota digitada")
-    });
+      .then((dados) => {
+        setTotalTitulos(dados.data.numLinhas);
+        console.log("fsad", dados.data.numLinhas);
+
+      }).catch((erro) => {
+        console.log("não foi possível recuperar os dados da rota digitada")
+      });
 
   }, [offset, page])
 
@@ -40,55 +40,60 @@ function Titulos() {
       colunas.push(x);
     }
   }
-  return (<>
-    <Pagination limit={LIMIT} total={totalTitulos} offset={offset} setOffset={setOffset} setPage={setPage}/>
+  return (<div>
 
-    <div className='container-fluid' style={{ "borderRadius": "15px", "margin": "1em", "height": "100%", "width": "98%", "padding": "10px", "backgroundColor": "#fff", "justifyContent": "center" }}>
+    <Pagination limit={LIMIT} total={totalTitulos} offset={offset} setOffset={setOffset} setPage={setPage} />
 
-      {dadosTitulos.length > 0 && (<table className='table table-striped table-hover' style={{ "backgroundColor": "#Fbfbfb", "borderRadius": "15px", "minWidth": "100%", "minHeight": "100%" }}>
-        <thead>
-          <tr>
-            {colunas.map((colunas, i) => { return (<th scope='row' className='col-1 text-center' key={i}>{colunas.toUpperCase()}</th>) })}
-          </tr>
-        </thead>
-        <tbody>
-          {dadosTitulos.map((elemento, i) => {
-            return (<tr>
-              {colunas.map((col,i) => {
-                if(i%6===0 && i>1){
-                  return (
-                    <td className={style.tdin + ' text-center col-1'}>
-                      {elemento[col].length > 40 ?  (Number(elemento[col]).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})).substring(0, 40) + "..." :(Number(elemento[col]).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))}
-  
-                    </td>);
-                }
-                if(i%7===0 && i>1){
-                  return (
-                    <td className={style.tdin + ' text-center col-1'}>
-                      {elemento[col].length > 40 ?  (Number(elemento[col]).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})).substring(0, 40) + "..." : (Number(elemento[col]).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))}
-  
-                    </td>);
-                }
-                if(i%8===0 && i>1){
-                  return (
-                    <td className={style.tdin + ' text-center col-1'}>
-                      {elemento[col].length > 40 ? (Number(elemento[col]).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})).substring(0, 40) + "..." : (Number(elemento[col]).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))}
-  
-                    </td>);
-                }
-                return (
-                  <td className={style.tdin + ' text-center col-1'}>
-                    {elemento[col].length > 40 ? elemento[col].substring(0, 40) + "..." : elemento[col]}
+    <div className={'container-fluid ' + style.div_container}>
 
-                  </td>);
+      {dadosTitulos.length > 0 && (
+        <div className='table-responsive'>
+          <table className='table table-striped table-hover'>
+            <thead>
+              <tr>
+                {colunas.map((colunas, i) => { return (<th scope='row' className='col-1 text-center' key={i}>{colunas.toUpperCase()}</th>) })}
+              </tr>
+            </thead>
+            <tbody>
+              {dadosTitulos.map((elemento, i) => {
+                return (<tr>
+                  {colunas.map((col, i) => {
+                    if (i % 6 === 0 && i > 1) {
+                      return (
+                        <td className={style.tdin + ' text-center col-1'}>
+                          {elemento[col].length > 40 ? (Number(elemento[col]).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })).substring(0, 40) + "..." : (Number(elemento[col]).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))}
+
+                        </td>);
+                    }
+                    if (i % 7 === 0 && i > 1) {
+                      return (
+                        <td className={style.tdin + ' text-center col-1'}>
+                          {elemento[col].length > 40 ? (Number(elemento[col]).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })).substring(0, 40) + "..." : (Number(elemento[col]).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))}
+
+                        </td>);
+                    }
+                    if (i % 8 === 0 && i > 1) {
+                      return (
+                        <td className={style.tdin + ' text-center col-1'}>
+                          {elemento[col].length > 40 ? (Number(elemento[col]).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })).substring(0, 40) + "..." : (Number(elemento[col]).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))}
+
+                        </td>);
+                    }
+                    return (
+                      <td className={style.tdin + ' text-center col-1'}>
+                        {elemento[col].length > 40 ? elemento[col].substring(0, 40) + "..." : elemento[col]}
+
+                      </td>);
+                  })}
+                </tr>)
               })}
-            </tr>)
-          })}
-        </tbody>
-      </table>)}
+            </tbody>
+          </table>
+        </div>
+      )}
 
     </div>
-  </>
+  </div>
 
   );
 }
