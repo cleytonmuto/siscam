4b114eefc598bc8475eb383e2656200129ac3589
fernandoig1209 <fernandoig1209@gmail.com>
2022-12-23 17:55:54 -0300

    style: página home alterada

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import style from './Home.module.css'
 function Home() {
   return (
-    <div className={'container-fluid ' + style.fundo}>
+    <div className={style.fundo}>
       <div className={'container-fluid ' + style.ca}>
         <div className={'container-fluid ' + style.backgroundimg}>
-            <p>Bem-Vindo!<br/>SISCAM-PGE/PA</p>
+            <p>Bem-vindo!<br/>SISCAM-PGE/PA</p>
        
 
 
         </div>
-      </div>
+      </div> 
+      
     </div>
   )
 }
