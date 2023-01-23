bbd6892dda4ec3d994e4fb1f249aea68e88fbe89
cleytonmuto <cleytonmuto@gmail.com>
2023-01-23 10:58:28 -0300

    refactor: pages/Dash.js

--- src/pages/Dash.js
+++ src/pages/Dash.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Chart } from "react-google-charts";
+import { Chart } from 'react-google-charts';
+
 function Dash(){
   return(<Chart
-    chartType="ScatterChart"
-    data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
-    width="100%"
-    height="400px"
+    chartType='ScatterChart'
+    data={[['Age', 'Weight'], [-4, 5.5], [8, 12]]}
+    width='100%'
+    height='400px'
     legendToggle
   />)
 }
+
 export default Dash;
