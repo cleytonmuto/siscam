04892518a51bb260bcf0e66160c0e0a2f673b22f
abudluan <abudluan@gmail.com>
2023-03-20 12:08:07 -0300

    feat: btn login added in home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import { Button } from 'react-bootstrap';
+import { FaSignInAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import './Home.scss'
 
@@ -7,6 +9,7 @@ const Home = () => {
   return (
     <div className='container textHome'>
       <h1>Bem-vindo (a) ao  <span className='text-SISCAM'>SISCAM-PGE/PA</span></h1>
+      <Button as={Link} to="/signin" className='btn-login my-4'><FaSignInAlt className="iconNav"/> Login</Button>
     </div>
   );
 }
